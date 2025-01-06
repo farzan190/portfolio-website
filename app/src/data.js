1c2d17caf00a78d594804d4233fd1da00c94326f
farzan190 <farzanxavier@gmail.com>
2025-01-06 13:28:47 +0530

    added projects and its descriptions

--- app/src/data.js
+++ app/src/data.js
@@ -1,6 +1,12 @@
 import pic from './assets/desktop-suggestions.png'
 import lift from './assets/lifts.jpeg'
 import port from './assets/portfolio.jpeg';
+import invoice from './assets/invoice.png';
+import form from './assets/form.png';
+import comingsoon from './assets/comingsoon.png';
+
+
+
 
 
 export default [
@@ -13,7 +19,20 @@ export default [
     githubLink:"https://github.com/farzan190/Product-feedback-Application",
   }
 ,
+
 { 
+  picture:invoice,
+  title:"InvoiceApp",
+  description:"Invoices App to create, track, manage invoices. Features status updates, edits, deletions. Built with a clean, responsive UI.",
+  liveLink:"https://invoice-app-ukdd.vercel.app/",
+  githubLink:"https://github.com/farzan190/-InvoiceApp",
+},{ 
+  picture:form,
+  title:"MultiStepForm",
+  description:"Multi-Step Form to collect user info across steps. Features progress display, validation. Built with a clean  UI.",
+  liveLink:"https://multi-step-form-13cj.vercel.app/",
+  githubLink:"https://github.com/farzan190/Multi-step-form",
+},{ 
   picture:lift,
   title:"Lift simulation",
   description:"Simulated a multi-floor lift system in VanillaJS, with dynamic lift responses based on real-time floor requests",
@@ -26,9 +45,15 @@ export default [
   description:"a user-friendly platform that empowers customers to submit feedback, suggest features, and vote on ideas.",
   liveLink:"https://product-feedback-application-nine.vercel.app/",
   githubLink:"https://github.com/farzan190/portfolio-website",
+},
+{ 
+  picture:comingsoon,
+  title:"Wordle",
+  description:"Wordle Game to guess hidden words. Features daily challenges, hints. Built with a responsive UI.",
+  liveLink:"",
+  githubLink:"",
 }
 ,
-
 ]
 
 
